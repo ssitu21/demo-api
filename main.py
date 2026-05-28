d110158c628d47f1677b444e061a766731969f9b
ssitu21 <shawns097@deltalearns.ca>
2026-05-28 12:43:54 -0700

    added loop to make things abit easier
    yes done everything good good

--- main.py
+++ main.py
@@ -5,28 +5,38 @@ We will use it to access currency exchange data.
 
 import requests
 
-# Ask user for currency information
-from_currency = input("Enter from currency: ").upper().strip()
-to_currency = input("Enter to currency: ").upper().strip()
-amount = float(input("Enter amount: "))
+while True:
 
-# Enter your API key here (if needed )
-API_KEY = "122a76de7e1339ad9a91fb11"
+    # Ask user for currency information
+    from_currency = input("Enter from currency: ").upper().strip()
+    to_currency = input("Enter to currency: ").upper().strip()
+    amount = float(input("Enter amount: "))
 
-# Generate the URL you will be requesting (read the documentation on the API site)
-URL = f"https://open.er-api.com/v6/latest/{from_currency}"
+    # Enter your API key here (if needed )
+    API_KEY = "122a76de7e1339ad9a91fb11"
 
-# Use Requests to get data from the URL back in JSON format
-# JSON = javascript object notation (you can also use XML but I prefer JSON)
-json_data = requests.get(URL, timeout=10).json()
+    # Generate the URL you will be requesting (read the documentation on the API site)
+    URL = f"https://open.er-api.com/v6/latest/{from_currency}"
 
-# Use brackets just like in lists and dictionaries to get the exact data you need
-# Use a JSON viewer like https://codebeautify.org/jsonviewer to read your JSON
-rate = json_data['rates'][to_currency]
+    # Use Requests to get data from the URL back in JSON format
+    # JSON = javascript object notation (you can also use XML but I prefer JSON)
+    json_data = requests.get(URL, timeout=10).json()
 
-# Convert currency
-converted = amount * rate
+    # Use brackets just like in lists and dictionaries to get the exact data you need
+    # Use a JSON viewer like https://codebeautify.org/jsonviewer to read your JSON
+    rate = json_data['rates'][to_currency]
 
-# Print the conversion
-print("Currency Exchange")
-print(f"{amount} {from_currency} = {converted:.2f} {to_currency}")
+    # Convert currency
+    converted = amount * rate
+
+    # Print the conversion
+    print("Currency Exchange")
+    print(f"{amount} {from_currency} = {converted:.2f} {to_currency}")
+
+    # Loop
+    
+    again = input("\nDo you want to convert again? (y/n):").lower().strip()
+
+    if again != "y":
+        print("Goodbye!")
+        break
